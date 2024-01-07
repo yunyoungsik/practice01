466ac80329d8e7ad69beccce844c98df5a129f1e
yunyoungsik <144635640+yunyoungsik@users.noreply.github.com>
2024-01-07 18:25:45 +0900

    20240107

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,21 +1,28 @@
 import '@styles/globals.css';
 
+import Nav from '@components/Nav';
+import Provider from '@components/Provider';
+
 export const metadata = {
-    title: "Promptopia",
-    description: 'Discover & Share Ai Prompts'
-}
+  title: 'Promptopia',
+  description: 'Discover & Share AI Prompts',
+};
 
-const RootLayout = ({children}) => {
-  return (
-    <html lang='ko'>
-        <div className='main'>
-            <div className='gradient' />
+const RootLayout = ({ children }) => (
+  <html lang="en">
+    <body>
+      <Provider>
+        <div className="main">
+          <div className="gradient" />
         </div>
-        <main className='app'>
-            {children}
-        </main>
-    </html>
-  )
-}
 
-export default RootLayout
+        <main className="app">
+          <Nav />
+          {children}
+        </main>
+      </Provider>
+    </body>
+  </html>
+);
+
+export default RootLayout;
---
+++ models/user.js
@@ -0,0 +1,24 @@
+import { Schema, model, models } from 'mongoose';
+
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    unique: [true, 'Email already exists!'],
+    required: [true, 'Email is required!'],
+  },
+  username: {
+    type: String,
+    required: [true, 'Username is required!'],
+    match: [
+      /^(?=.{2,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._가-힣]+(?<![_.])$/,
+      'Username invalid, it should contain 2-20 alphanumeric or Korean letters and be unique!',
+    ],
+  },
+  image: {
+    type: String,
+  },
+});
+
+const User = models.User || model('User', UserSchema);
+
+export default User;
